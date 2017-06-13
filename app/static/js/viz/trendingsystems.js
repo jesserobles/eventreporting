af71c3506cccbeb64fdadbe866792d90572b7a6b
Jesse Robles <jesse.robles@gmail.com>
2017-06-12 20:52:19 -0400

    Added edit capabilities

--- app/static/js/viz/trendingsystems.js
+++
@@ -1,7 +0,0 @@
-trendingsystems = []
-d3.tsv("data/trendingsystems.tsv", function(error, systems) {
-  if (error) return console.error(error);
-  systems.forEach(function(d) {
-    trendingsystems.push(d['Systems'])
-  })
-});
